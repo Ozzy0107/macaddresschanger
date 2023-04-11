e1cc3877c6328b1055937b162c5ec8a85a5f513a
Gabriel Ozzy <102391831+Ozzy0107@users.noreply.github.com>
2023-04-11 10:22:36 -0400

    110423.001

---
+++ macaddresschanger.py
@@ -0,0 +1,38 @@
+import subprocess
+import argparse
+import textwrap
+
+class MacChanger:
+    def __init__(self,args):
+        self.args = args
+
+
+    try:
+        def changeMac(self):
+            print(f"Changing MAC address for {self.args.interface}\n.Setting the new value {self.args.address}")
+            subprocess.call(["ifdown", self.args.interface])
+            subprocess.call(["ifconfig", self.args.interface, "hw", "ether", self.args.address])
+            subprocess.call(["ifup", self.args.interface])
+    except Exception as e:
+        print(f"Error {e}")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description = 'MAC Address changer',
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        epilog=textwrap.dedent(
+            '''
+            Example: python macaddresschanger.py -i eth0 -m 00:11:22:33:44:22
+            '''
+        ))
+    parser.add_argument('-i', '--interface', help='Interface to change the MAC Address')
+    parser.add_argument('-m', '--address', help='New MAC Address')
+
+    args = parser.parse_args()
+
+    if not (args.address and args.interface):
+        parser.print_help()
+
+    macChanger = MacChanger(args)
+    macChanger.changeMac()
